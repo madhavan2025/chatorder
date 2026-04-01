a9bdf9d2eadb0a477782e6234d965371892a7e7d
madhavan2025 <madhavan.sykasys@gmail.com>
2026-04-01 16:37:20 +0530

    upsunsssdwwated changewww0000000saassww44wazxcsavmmaafcvaaasxcnsxasdwasaxcaafvbasxzxdxs

--- components/EmbedChatHeader.tsx
+++ components/EmbedChatHeader.tsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from "react";
 export function EmbedChatHeader() {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [isFullScreen, setIsFullScreen] = useState(false);
+  const [chatTheme, setChatTheme] = useState<any>(null);
+  const [loadingTheme, setLoadingTheme] = useState(true);
 
   useEffect(() => {
     if (isDarkMode) {
@@ -12,6 +14,27 @@ export function EmbedChatHeader() {
     } else {
       document.documentElement.classList.remove("dark");
     }
+  }, [isDarkMode]);
+
+ useEffect(() => {
+    const loadTheme = async () => {
+      const userId = "150001";
+      try {
+        const themeName = isDarkMode ? "darkTheme" : "default";
+
+        const res = await fetch(
+          `/api/chat-theme?theme=${themeName}&userId=${userId}`
+        );
+        const data = await res.json();
+        setChatTheme(data);
+      } catch (err) {
+        console.error("Theme load failed", err);
+      } finally {
+        setLoadingTheme(false);
+      }
+    };
+
+    loadTheme();
   }, [isDarkMode]);
 
   const closeChat = () => {
@@ -30,18 +53,26 @@ export function EmbedChatHeader() {
 
   return (
     <div className="h-14 bg-[#1d4ed8] dark:bg-[#111827] text-white flex items-center justify-between px-4 shrink-0">
-      <span className="font-semibold">Sykalab-AI-ShopAgent</span>
+       <span className="font-semibold">
+        {loadingTheme ? "Loading..." : chatTheme?.agentName || "Assistant"}
+      </span>
 
       <div className="flex items-center gap-3">
-        <button onClick={() => setIsDarkMode(!isDarkMode)}>
+        <button 
+         className="cursor-pointer"
+        onClick={() => setIsDarkMode(!isDarkMode)}>
           {isDarkMode ? <Sun size={18} /> : <Moon size={18} />}
         </button>
 
-        <button onClick={toggleFullScreen}>
+        <button 
+         className="cursor-pointer"
+        onClick={toggleFullScreen}>
           {isFullScreen ? <Minimize2 size={18} /> : <Maximize2 size={18} />}
         </button>
 
-        <button onClick={closeChat}>
+        <button 
+         className="cursor-pointer"
+        onClick={closeChat}>
           <X size={20} />
         </button>
       </div>
